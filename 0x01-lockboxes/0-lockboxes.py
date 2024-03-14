6dc075255f9a3eda51e7e72cd1c5bffe69141aba
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-14 19:39:58 +0000

    solve lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,6 +3,16 @@
 
 
 def canUnlockAll(boxes):
-    """ Check if boxes can be unlocked."""
+    """Check if boxes can be unlocked."""
+    opened_boxes = set()
+    opened_boxes.add(0)
 
-    return True
+    def dfs(box_index):
+        for key in boxes[box_index]:
+            if key not in opened_boxes:
+                opened_boxes.add(key)
+                dfs(key)
+
+    dfs(0)
+
+    return len(opened_boxes) == len(boxes)

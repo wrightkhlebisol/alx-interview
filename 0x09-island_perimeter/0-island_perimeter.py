11113571ef2919384d866acc84c8b009763755e5
wrightkhlebisol <calebogundiya@gmail.com>
2024-10-19 09:04:16 +0100

    fix lint errors

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-"""
-Calculate perimter of island
-"""
+
 def island_perimeter(grid):
+    """
+    Calculate perimter of island
+    """
     perimeter = 0
+
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                perimeter += check_sides(x,y,grid)
+                perimeter += check_sides(x, y, grid)
+
     return perimeter
 
-"""
-Check closed sides on given island
-"""
+
+
 def check_sides(x, y, grid):
+    """
+    Check closed sides on given island
+    """
     closed_sides = 0
     sides = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
     for side in sides:
         if grid[x - side[0]][y - side[1]] == 0:
             closed_sides += 1
+
     return closed_sides
 
